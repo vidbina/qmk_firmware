7561e549870fbef4ddc738ec96eb65497594e933
David Asabina <vid@bina.me>
2023-03-12 18:08:56 +0000

    chore: track mugur-generated Charybdis config

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mugur-emacs-vidbina/rules.mk
@@ -0,0 +1,9 @@
+ # START-MUGUR-REGION
+
+ FORCE_NKRO       = yes
+     LEADER_ENABLE    = no
+     RGBLIGHT_ENABLE  = no
+     TAP_DANCE_ENABLE = no
+     COMBO_ENABLE     = no 
+
+# END-MUGUR-REGION           
