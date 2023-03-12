8517c3b7066961471739462e8c3d7238c9faa358
David Asabina <vid@bina.me>
2023-03-12 18:08:56 +0000

    chore: track mugur-generated Charybdis config

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mugur-emacs-vidbina/config.h
@@ -0,0 +1,12 @@
+ // START-MUGUR-REGION
+
+ #undef TAPPING_TERM
+     #define TAPPING_TERM 180
+     #define COMBO_TERM 300
+     #define LEADER_TIMEOUT 300
+     #define      LEADER_PER_KEY_TIMING
+     #define COMBO_COUNT 0
+     #define FORCE_NKRO
+     #undef RGBLIGHT_ANIMATIONS 
+
+// END-MUGUR-REGION           
