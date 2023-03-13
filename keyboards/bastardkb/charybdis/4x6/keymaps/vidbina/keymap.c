9c972cf4968f58e715e44b35c7395e75840f403f
David Asabina <vid@bina.me>
2023-03-13 06:21:13 +0000

    chore: clear Alt on base-layer (to encourage cluster use)

--- keyboards/bastardkb/charybdis/4x6/keymaps/vidbina/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vidbina/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
+       KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_BSPC, SPR_SPC,   LOWER,      RAISE, SPR_ENT,
                                            KC_LCTL, SC_LAPO,    SC_RSPC
