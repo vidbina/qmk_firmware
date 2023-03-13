05739391ffea57b3a27f6a773c2a4f003c4a13a7
David Asabina <vid@bina.me>
2023-03-13 06:18:59 +0000

    feat: place backspace on base of left thumb cluster & right board

--- keyboards/bastardkb/charybdis/4x6/keymaps/vidbina/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vidbina/keymap.c
@@ -63,7 +63,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_LGUI, SPR_SPC,   LOWER,      RAISE, SPR_ENT,
-                                           KC_LCTL, SC_LAPO,    SC_RAPC
+                                  KC_BSPC, SPR_SPC,   LOWER,      RAISE, SPR_ENT,
+                                           KC_LCTL, SC_LAPO,    SC_RSPC
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
